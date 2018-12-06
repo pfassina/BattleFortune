4e14b61e1c4f011df63fef2acfd93dd7670f59c1
pfassina <pfassina@gmail.com>
2018-12-05 20:54:30 -0800

    added function description

--- decode.py
+++ decode.py
@@ -1,6 +1,11 @@
 import yaml
 
 def nation(id, attribute='name'):
+    '''
+    Decodes Nation Id.
+    Takes Nation Id as input, and returns nation attribute.
+    Nation attributes:  name, abbreviation, epithet, file_name, era
+    '''
 
     output = yaml.load(open('./data/nations.yaml', 'r'))[id][attribute]
     return output
