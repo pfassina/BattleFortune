3aaa0dcddb32e967a49cefac711587ff4e577241
pfass <pfassina@gmail.com>
2020-06-13 10:37:37 -0700

    mac  version

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from src import battlefortune
 
 if __name__ == '__main__':
 
-    kwargs = {
-        'game': 'test',
-        'simulations': 5,
-        'province': 6,
-    }
+    dp = input('dominions 5 executable path: ')
+    gp = input('saved games path: ')
 
-    battlefortune.startup(**kwargs)
+    battlefortune.setup(dom_path=dp, game_path=gp)
+
+    gn = input('game name: ')
+    sr = input('simulation rounds: ')
+    pn = input('province number: ')
+
+    battlefortune.startup(game=gn, simulations=sr, province=pn)
     battlefortune.battlefortune()
