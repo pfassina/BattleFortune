32c963feb063d3e8a0ac9b60595a92ca6c54f79a
faspaulo <faspaulo@amazon.com>
2022-02-04 11:45:42 -0800

    updating packages

--- src/globals.py
+++ src/globals.py
@@ -16,5 +16,22 @@ def init():
     LOGS = None
 
     WIN_COUNTS = None
+    ARMY_COSTS = None
+    ATTACKER_DF = None
+    DEFENDER_DF = None
+
+
+if __name__ == "__main__":
+
+    DOM_PATH = None
+    GAME_PATH = None
+    SIMULATIONS = None
+    GAME_NAME = None
+    PROVINCE = None
+    FAILED_ROUNDS = None
+    VALID_ROUNDS = None
+    LOGS = None
+    WIN_COUNTS = None
+    ARMY_COSTS = None
     ATTACKER_DF = None
     DEFENDER_DF = None
